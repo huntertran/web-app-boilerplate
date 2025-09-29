e638e02aed8e591be4d245db5cb7e158a5684580
n1340t <nguyenbao1003@hotmail.com>
2025-09-28 21:32:03 -0400

    Bug: UserItem table not found

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Users;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(opt => opt.UseSqlite("Users"));
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
@@ -12,6 +16,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
 
 app.UseHttpsRedirection();
